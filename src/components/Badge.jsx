b0f02dfbcc467a88f490012214be1cd808b55be9
Faiz <moch.choirulfaiz2008@gmail.com>
2025-09-01 13:38:43 +0700

    adjustment and update 1/09/25 v11

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,7 +1,9 @@
 export default function Badge({ children }) {
   return (
     <>
-      <div className="bg-purple-600/20 px-4 py-1  rounded-full">{children}</div>
+      <div className="from-purple-600/20 via-purple-300/50 to-purple-600/20 bg-gradient-to-br px-4 py-1 text-white  rounded-full">
+        {children}
+      </div>
     </>
   );
 }

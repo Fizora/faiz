19a8d4871d64fabcbc9f47ef1cf5b1585e5db749
Faiz <moch.choirulfaiz2008@gmail.com>
2025-09-01 15:28:36 +0700

    adjustment and update 1/09/25 v14

---
+++ src/app/letters/letters2/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+import Link from "next/link";
+import Sidebar from "@/components/Sidebar";
+import Title from "@/components/Title";
+import { ArrowLeft } from "lucide-react";
+
+export default function Letters2() {
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row max-w-4xl mx-auto">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Konten utama */}
+      <main className="flex-1 px-4 md:px-10 py-10 text-zinc-400 ">
+        <section className="py-30 space-y-6">
+          <Link
+            href={"/letters"}
+            className="flex items-center gap-2 hover:text-white"
+          >
+            <ArrowLeft size={20} />
+            Back
+          </Link>
+          <div className="py-10 space-y-6">
+            <Title>How I learning Programming?</Title>
+            <p>
+              I write my experiences, reflections and learnings here, follow my
+              progress
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
--- src/app/sosmed/page.jsx
+++ src/app/sosmed/page.jsx
@@ -1,6 +1,15 @@
 "use client";
+import Link from "next/link";
 import Sidebar from "@/components/Sidebar";
-import Development from "@/components/Development";
+import { FaInstagram, FaLinkedin, FaTiktok, FaYoutube } from "react-icons/fa";
+import Button from "@/components/Button";
+import Title from "@/components/Title";
+const sosmed = [
+  { icon: <FaTiktok />, name: "Tiktok", link: "" },
+  { icon: <FaYoutube />, name: "YouTube", link: "" },
+  { icon: <FaInstagram />, name: "Instagram", link: "" },
+  { icon: <FaLinkedin />, name: "Linkedin", link: "" },
+];
 
 export default function SocialMedia() {
   return (
@@ -9,17 +18,24 @@ export default function SocialMedia() {
       <Sidebar />
 
       {/* Konten utama */}
-      <div className="flex-1 overflow-y-auto min-h-screen flex items-center justify-center p-6">
-        <div className="text-center space-y-6">
-          {/* Komponen Development */}
-          <Development />
-
-          {/* Info tambahan */}
-          <p className="text-zinc-500">
-            This page is currently under development. Stay tuned for updates! 🚧
-          </p>
-        </div>
-      </div>
+      <main className="flex-1 px-4 md:px-10 py-10 text-zinc-400 ">
+        <section className="py-30 space-y-6">
+          <Title>My Social Media</Title>
+          {sosmed.map((item) => (
+            <div className="my-5 rounded-sm border border-zinc-800 bg-zinc-900 flex items-center justify-between p-2">
+              <div className="flex items-center gap-2">
+                <span>{item.icon}</span>
+                <h1>{item.name}</h1>
+              </div>
+              <Button>
+                <a href="" target="_blank">
+                  Visit
+                </a>
+              </Button>
+            </div>
+          ))}
+        </section>
+      </main>
     </div>
   );
 }

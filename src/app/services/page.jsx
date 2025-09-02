5f125e2b52879401c838cf526e40f3a6125dfabb
Faiz <moch.choirulfaiz2008@gmail.com>
2025-09-02 14:00:54 +0700

    add page services v2 fix bug

---
+++ src/app/services/page.jsx
@@ -0,0 +1,25 @@
+"use client";
+import Sidebar from "@/components/Sidebar";
+import Development from "@/components/Development";
+
+export default function Service() {
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row max-w-4xl mx-auto">
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Konten utama */}
+      <div className="flex-1 overflow-y-auto min-h-screen flex items-center justify-center p-6">
+        <div className="text-center space-y-6">
+          {/* Komponen Development */}
+          <Development />
+
+          {/* Info tambahan */}
+          <p className="text-zinc-500">
+            This page is currently under development. Stay tuned for updates! 🚧
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
